7e8346518a2d69057e3101e90cceba2f89c24efd
KyuBlade <jonesadev@gmail.com>
2017-02-25 18:54:49 +0100

    Fixed ping command

--- src/main/java/com/omega/command/impl/PingCommand.java
+++ src/main/java/com/omega/command/impl/PingCommand.java
@@ -12,25 +12,16 @@ import sx.blah.discord.util.DiscordException;
 import sx.blah.discord.util.MissingPermissionsException;
 import sx.blah.discord.util.RateLimitException;
 
-@Command(name = "ping", aliases = "pg")
+@Command(name = "ping")
 public class PingCommand extends AbstractCommand {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(PingCommand.class);
 
     public PingCommand(IUser by, IMessage message) {
         super(by, message);
     }
 
-    @Signature(help = "Send a ping to the bot to know if he is online. Also print the daily API response time.")
+    @Signature(help = "Send a ping to the bot to know if he is online and get the response time")
     public void pingCommand() throws RateLimitException, DiscordException, MissingPermissionsException {
-        String ping = null;
-        try {
-            DiscordStatus.getAPIResponseTimeForDay();
-        } catch (Exception e) {
-            LOGGER.warn("Unable to get API response time", e);
-            ping = "N/A";
-        }
-
-        by.getOrCreatePMChannel().sendMessage("Pong - Today API response time : " + ping);
+        long ping = message.getShard().getResponseTime();
+        by.getOrCreatePMChannel().sendMessage(String.format("Pong - %dms", +ping));
     }
 }
