61e79873d4f20fc67eea31deb5907a5b377ac5b7
KyuBlade <jonesadev@gmail.com>
2017-02-26 18:51:33 +0100

    No need to delete message in the callback anymore

--- src/main/java/com/omega/audio/callback/PlayAudioLoadCallback.java
+++ src/main/java/com/omega/audio/callback/PlayAudioLoadCallback.java
@@ -5,16 +5,9 @@ import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import sx.blah.discord.handle.obj.IMessage;
-import sx.blah.discord.util.DiscordException;
-import sx.blah.discord.util.MissingPermissionsException;
-import sx.blah.discord.util.RateLimitException;
 
 public class PlayAudioLoadCallback implements AudioLoadResultHandler {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(PlayAudioLoadCallback.class);
 
     private final IMessage message;
 
@@ -24,34 +17,21 @@ public class PlayAudioLoadCallback implements AudioLoadResultHandler {
 
     @Override
     public void trackLoaded(AudioTrack track) {
-        deleteMessage();
+
     }
 
     @Override
     public void playlistLoaded(AudioPlaylist playlist) {
         MessageUtil.reply(message, "Will play playlist {}({} tracks).");
-        deleteMessage();
     }
 
     @Override
     public void noMatches() {
         MessageUtil.reply(message, "No tracks found.");
-        deleteMessage();
     }
 
     @Override
     public void loadFailed(FriendlyException exception) {
         MessageUtil.reply(message, "Everything exploded, try again or not.");
-        deleteMessage();
-    }
-
-    private void deleteMessage() {
-        try {
-            message.delete();
-        } catch (MissingPermissionsException e) {
-            LOGGER.warn("Permission needed : {}", e.getMissingPermissions());
-        } catch (RateLimitException | DiscordException e) {
-            LOGGER.warn("Unable to delete message", e);
-        }
     }
 }
--- src/main/java/com/omega/audio/callback/QueueAudioLoadCallback.java
+++ src/main/java/com/omega/audio/callback/QueueAudioLoadCallback.java
@@ -5,16 +5,9 @@ import com.sedmelluq.discord.lavaplayer.player.AudioLoadResultHandler;
 import com.sedmelluq.discord.lavaplayer.tools.FriendlyException;
 import com.sedmelluq.discord.lavaplayer.track.AudioPlaylist;
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import sx.blah.discord.handle.obj.IMessage;
-import sx.blah.discord.util.DiscordException;
-import sx.blah.discord.util.MissingPermissionsException;
-import sx.blah.discord.util.RateLimitException;
 
 public class QueueAudioLoadCallback implements AudioLoadResultHandler {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(QueueAudioLoadCallback.class);
 
     private final IMessage message;
 
@@ -25,36 +18,22 @@ public class QueueAudioLoadCallback implements AudioLoadResultHandler {
     @Override
     public void trackLoaded(AudioTrack track) {
         MessageUtil.reply(message, "Track " + track.getInfo().title + " added to queue");
-        deleteMessage();
     }
 
     @Override
     public void playlistLoaded(AudioPlaylist playlist) {
         MessageUtil.reply(message, "Playlist " + playlist.getName() + " (" + playlist.getTracks().size() + " tracks) added to queue");
-        deleteMessage();
     }
 
     @Override
     public void noMatches() {
         MessageUtil.reply(message, "The provided source may not be supported");
-        deleteMessage();
     }
 
     @Override
     public void loadFailed(FriendlyException exception) {
         if (!exception.severity.equals(FriendlyException.Severity.COMMON)) {
             MessageUtil.reply(message, "Unable to add to queue");
-            deleteMessage();
-        }
-    }
-
-    private void deleteMessage() {
-        try {
-            message.delete();
-        } catch (MissingPermissionsException e) {
-            LOGGER.warn("Permission needed : {}", e.getMissingPermissions());
-        } catch (RateLimitException | DiscordException e) {
-            LOGGER.warn("Unable to delete message", e);
         }
     }
 }
