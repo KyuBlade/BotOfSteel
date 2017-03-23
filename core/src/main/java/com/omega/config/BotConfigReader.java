7915884917809b472c6a7a2df768992b32b6a14e
KyuBlade <jonesadev@gmail.com>
2017-03-23 15:35:22 +0100

    Add config package

---
+++ core/src/main/java/com/omega/config/BotConfigReader.java
@@ -0,0 +1,21 @@
+package com.omega.config;
+
+import org.w3c.dom.Document;
+
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactoryConfigurationException;
+
+public class BotConfigReader extends AbstractConfigReader<BotConfig> {
+
+    public BotConfigReader(BotConfig config) {
+        super(config);
+    }
+
+    @Override
+    protected void read(Document document)
+        throws XPathFactoryConfigurationException, XPathExpressionException {
+        config.setBotToken(readString(document, "//BotToken"));
+        config.setClientId(readString(document, "//ClientId"));
+        config.setShards(readNumber(document, "//Shards").intValue());
+    }
+}
